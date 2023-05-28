caacedb5ba771cab2c545ed2c9409415babbeb49
dchorn <denis.man.07@gmail.com>
2023-05-29 01:15:24 +0200

    fixed why it was not reading correctly

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,17 +2,17 @@
  * VibeVisor Discord Bot - commands - ping
  * https://github.com/dchorn/VibeVisor
  *
- * This script contains the definition for the slash command ping.
+ * This script contains the definition for the slash command ping..
  *
  * Developed by: Denys Chorny Savchuk
- * Date: 28/05/2023
+ * Date: 27/05/2023
  *
  * This file is the main entry point for the bot.
  */
 
 const { SlashCommandBuilder } = require("discord.js");
 
-module.export = {
+module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
