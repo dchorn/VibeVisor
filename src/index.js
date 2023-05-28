fafa6e09cae71062c5bb7a74dba4cf111546bb53
dchorn <denis.man.07@gmail.com>
2023-05-29 01:15:35 +0200

    Loading command files

--- src/index.js
+++ src/index.js
@@ -15,12 +15,24 @@ const path = require('path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('../config.json');
 
-
-
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds ]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ('data' in command && 'execute' in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+	}
+}
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the events parameter to keep it separate from the alredy defined 'client'
@@ -28,9 +40,6 @@ client.once(Events.ClientReady, c => {
     console.log(`VibeVisor is online!`);
     console.log(`Logged in as ${c.user.tag}`);
 });
-
-// Handle messages
-client.on('message', handleMessages);
 
 // Log in to Discord with your client's token
 client.login(token)
